19b12d14eb27ec6041231e9c8a1df24ba9e11c7e
Graceiscoming <naratip.suwanich@g.swu.ac.th>
2026-04-09 16:57:51 +0700

    Revert "change ระบบ เพิ่ม"
    This reverts commit 6631f7e900e783f1175d472b50f2f367984ff810.

--- models/category.js
+++
@@ -1,8 +0,0 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
-
-const Category = sequelize.define('Category', {
-  CategoryName: { type: DataTypes.STRING, allowNull: false }
-});
-
-module.exports = Category;
